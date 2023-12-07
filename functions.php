deaa95824c9d4ff08c93e3f47e6dbd84f6b31394
noedmw <noe.dwwm48@gmail.com>
2023-12-07 11:58:35 +0100

    finito

--- functions.php
+++ functions.php
@@ -7,3 +7,93 @@ function register_my_menu(){
 add_action( 'after_setup_theme', 'register_my_menu' );
 
 ?>
+<?php
+add_theme_support( 'post-thumbnails' );
+
+// Création du CPT Apprennant
+
+function apprenants2_register_post_types(){
+
+  // CPT Apprenants
+  $labels_apprenants2 = array(
+
+    'name' => 'apprenants2',
+    'all items' => 'Tous les apprenants2', // Nom affiché dans le sous-menu
+    'singular_name' => 'Apprenants2',
+    'add_new_item' => 'Ajouter un apprenant2',
+    'edit _item' => 'Modifier apprenants2',
+    'menu_name' => 'Apprenants2'
+
+  );
+
+  $args = array(
+
+
+    'labels' => $labels_apprenants2,
+    'public' => true,
+    'show_in_rest' => true,
+    'has_archive' => true,
+    'supports' => array( 'title', 'editor', 'thumbnail' ),
+    'menu_position' => 5,
+    'menu_icon' => 'dashicons-admin-customizer'
+
+  );
+
+  register_post_type( 'apprenants2', $args );
+
+  // Déclaration de la Taxonomie1
+  $labels_apprenants2 = array(
+
+    'name' => 'competences',
+    'new_item_name' =>'Nom du nouveau Projet',
+    'parent_item' => 'Type de projet parent',
+    
+  );
+  
+  $args = array(
+
+    'labels' => $labels_apprenants2,
+    'public' => true,
+    'show_in_rest' => true,
+    'hierarchical' => false,
+
+  );
+
+
+
+  register_taxonomy( 'competences', 'apprenants2', $args );
+
+  $labels_apprenants2 = array(
+
+    'name' => 'Promotion',
+    'new_item_name' =>'Nom du nouveau Projet',
+    'parent_item' => 'Type de projet parent',
+  );
+  $args = array(
+
+    'labels' => $labels_apprenants2,
+    'public' => true,
+    'show_in_rest' => true,
+    'hierarchical' => true,
+
+  );
+  register_taxonomy( 'Promotion', 'apprenants2', $args );
+
+  $labels_apprenants2 = array(
+
+    'name' => 'Formation',
+    'new_item_name' =>'Nom du nouveau Projet',
+    'parent_item' => 'Type de projet parent',
+  );
+  $args = array(
+
+    'labels' => $labels_apprenants2,
+    'public' => true,
+    'show_in_rest' => true,
+    'hierarchical' => true,
+
+  );
+  register_taxonomy( 'Formation', 'apprenants2', $args );
+
+}
+add_action( 'init', 'apprenants2_register_post_types' ); //Le hook init lance la fonction
